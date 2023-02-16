b6abe42d5fc72197c0fcce3c017325026d611690
Anniessa Antar <anniessa.antar@gmail.com>
2023-02-16 17:13:32 -0600

    added images and made data file

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/me_n_zackaria.jpg', description: 'I love you Zackaria! A photo of me at age 10 and my sibling, Zackaria, at age 6 sitting next to each other.', likes: 0 },
+    { id: 2, path: 'images/cohortkitty.png', description: 'Nothing but love for my Amethyst cohort kitties! This is an AI-rendered photo of a purple kitten standing next to an amethyst.', likes: 0 },
+    { id: 3, path: 'images/friendship.jpg', description: 'Friendship is so important! This is a picture of three human-like figures high-fiving each other.', likes: 0 },
+    { id: 4, path: 'images/sahara_desert.jpeg', description: 'I truly feel like a desert creature. A photo of the Sahara at night.', likes: 0 },
+    { id: 4, path: 'images/kitties.jpeg', description: 'A photo of my two cats, Chicken Noodle and Bert&Ernie.', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
