d403bd59fd38ae36144458c2b67c7db5bb6caf5f
Anniessa Antar <anniessa.antar@gmail.com>
2023-02-16 17:15:27 -0600

    updated id numbers

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -3,7 +3,7 @@ const galleryItems = [
     { id: 2, path: 'images/cohortkitty.png', description: 'Nothing but love for my Amethyst cohort kitties! This is an AI-rendered photo of a purple kitten standing next to an amethyst.', likes: 0 },
     { id: 3, path: 'images/friendship.jpg', description: 'Friendship is so important! This is a picture of three human-like figures high-fiving each other.', likes: 0 },
     { id: 4, path: 'images/sahara_desert.jpeg', description: 'I truly feel like a desert creature. A photo of the Sahara at night.', likes: 0 },
-    { id: 4, path: 'images/kitties.jpeg', description: 'A photo of my two cats, Chicken Noodle and Bert&Ernie.', likes: 0 }
+    { id: 5, path: 'images/kitties.jpeg', description: 'A photo of my two cats, Chicken Noodle and Bert&Ernie.', likes: 0 }
 
 ];
 
